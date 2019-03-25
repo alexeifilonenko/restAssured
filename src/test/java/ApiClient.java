85cce96e93f0cd732be1f7c719b055d666b1dbf4
alexeifilonenko <alexeifilonenko@gmail.com>
2019-03-25 17:35:23 +0300

    new commit

--- src/test/java/ApiClient.java
+++ src/test/java/ApiClient.java
@@ -8,17 +8,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.*;
+
 import com.google.gson.GsonBuilder;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 
-import static io.restassured.RestAssured.when;
-import static io.restassured.config.RestAssuredConfig.config;
-import static sun.net.ftp.impl.FtpClient.create;
 
 public class ApiClient {
-    public String sessionToken = "9b64d6b8ef5288c57af9a4a308b15dc537514a5c81f3036b65537d85affcdc8c5bd6ff7701cf9b61d3becb48";
+    public String sessionToken;
     private final String baseUrl = "https://stage-platform.kino-mo.com";
     private final String pathSession = "/api/admin/0/session";
     private final String pathClient = "/api/admin/0/client/";
@@ -28,35 +26,93 @@ public class ApiClient {
     public String putSessionToken() throws JSONException {
 
         JSONObject requestBody = new JSONObject();
-        requestBody.put("username", "fonenko@hypervsn.com");
-        requestBody.put("password", "Filonko.com");
-        System.out.println(requestBody);
+        requestBody.put("username", "nenko@hypervsn.com");
+        requestBody.put("password", "nen-ko.com");
+        //System.out.println(requestBody);
 
         RequestSpecification requets = RestAssured.given()
                 .header("Content-Type", "application/json")
                 .body(requestBody.toString());
 
         Response response = requets.put(baseUrl + pathSession);
-        int statusCode = response.getStatusCode();
-        int successCode = response.jsonPath().get("status");
 
         JSONObject jsonResponse = new JSONObject(response.asString());
         //System.out.println(jsonResponse);
+        sessionToken = jsonResponse.getJSONObject("message").getString("sessionToken");
         System.out.println("sessionToken: " + sessionToken);
-        String sessionToken = jsonResponse.getJSONObject("message").getString("sessionToken");
         return sessionToken;
     }
 
     public String createClient() {
         JSONObject requestBody = new JSONObject();
-        requestBody.put("name", "t1e1s11t111213456719");
-        requestBody.put("legalName", "te1s1t12134156789");
+        requestBody.put("name", "t11e1s111");
+        requestBody.put("legalName", "t1e11s");
 
         JSONArray jsonArrayPhone = new JSONArray();
         jsonArrayPhone.put(0, "+1234156789");
 
         JSONArray jsonArrayEmail = new JSONArray();
-        jsonArrayEmail.put(0, "te111st111123@test1123.test123");
+        jsonArrayEmail.put(0, "te111113@test1123.test123");
+
+        requestBody.put("phone", jsonArrayPhone);
+        requestBody.put("email", jsonArrayEmail);
+
+        List<Double> coordinates = new ArrayList<>(Arrays.asList(51.5073509, -0.12775829999998223));
+
+        JSONObject defaultLocation = new JSONObject();
+        defaultLocation.put("type", "Point");
+        defaultLocation.put("coordinates", coordinates);
+        requestBody.put("defaultLocation", defaultLocation);
+
+        JSONArray allowedPermissions = new JSONArray();
+        allowedPermissions.put(0, "user:user:create:general");
+        requestBody.put("allowedPermissions", allowedPermissions);
+        //System.out.println(requestBody);
+
+        RequestSpecification requets = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("km-auth", sessionToken)
+                .body(requestBody.toString());
+
+        Response response = requets.put(baseUrl + pathClient);
+
+        JSONObject jsonResponse = new JSONObject(response.asString());
+        //System.out.println(jsonResponse);
+        clientId = jsonResponse.getJSONObject("message").getString("_id");
+        System.out.println("Id created client: " + clientId);
+        return clientId;
+    }
+
+    public void getClientById(String clientId) {
+
+        RequestSpecification requets = given()
+                .baseUri(baseUrl)
+                .basePath(pathClient + clientId)
+                .header("Content-Type", "application/json")
+                .header("km-auth", sessionToken);
+
+
+        Response response = requets.when().get();
+
+
+        JSONObject jsonObject = new JSONObject(response.asString());
+        String returnClient = jsonObject.getJSONObject("message").getString("_id");
+        System.out.println(returnClient);
+        Client client = response.jsonPath().getObject("message", Client.class);
+        System.out.println("Client ID: " + client.get_id() + " Client name: " + client.getName());
+    }
+
+    public String updateClient(String clientId) {
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("name", "t1e1s11t11121347777");
+        requestBody.put("legalName", "te1s1t12134177777");
+
+        JSONArray jsonArrayPhone = new JSONArray();
+        jsonArrayPhone.put(0, "+1234156789");
+
+        JSONArray jsonArrayEmail = new JSONArray();
+        jsonArrayEmail.put(0, "te111st111123@test1123.test7777");
 
         requestBody.put("phone", jsonArrayPhone);
         requestBody.put("email", jsonArrayEmail);
@@ -78,42 +134,11 @@ public class ApiClient {
                 .header("km-auth", sessionToken)
                 .body(requestBody.toString());
 
-        Response response = requets.put(baseUrl + pathClient);
+        Response response = requets.post(baseUrl + pathClient + clientId);
 
         JSONObject jsonResponse = new JSONObject(response.asString());
         System.out.println(jsonResponse);
-        String clientId = jsonResponse.getJSONObject("message").getString("_id");
-        System.out.println("Id created client " + clientId);
+
         return clientId;
-    }
-
-    public void getClientById(String clientId) {
-        /*RestAssured.config = RestAssured.config()
-                .objectMapperConfig(ObjectMapperConfig.objectMapperConfig()
-                        .gsonObjectMapperFactory( (type, charset) -> new GsonBuilder().create() ));*/
-
-        RequestSpecification requets = given()
-                .baseUri(baseUrl)
-                .basePath(pathClient + clientId)
-                .header("Content-Type", "application/json")
-                .header("km-auth", "d58736104ac1b62f240efcd069b502cd6c3ee12d17d0f8737ce274393a39f4b95bd6ff7701cf9b61d3becb48");
-
-
-        Response response = requets.when().get();
-
-
-        JSONObject jsonObject = new JSONObject(response.asString());
-        System.out.println(response.asString());
-        System.out.println(jsonObject);
-        //List<Client> returnClient = Arrays.asList(response.getBody().as(Client[].class));
-        String returnClient = jsonObject.getJSONObject("message").getString("_id");
-        System.out.println(returnClient);
-
-        // List<Client> clientResponse = response.jsonPath().getList("message", Client.class);
-        //System.out.println(clientResponse);
-        Client client = response.jsonPath().getObject("message", Client.class);
-        System.out.println(client.get_id() + client.getName());
-
-
     }
 }
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,10 +2,12 @@ public class Main {
     public static void main(String[] args) {
         ApiClient apiClient = new ApiClient();
 
-       //apiClient.putSessionToken();
-
+        apiClient.putSessionToken();
+        System.out.println("---------------------------------");
         //apiClient.createClient();
 
         apiClient.getClientById("5c9238330fb2d32bc288f1ea");
+        System.out.println("---------------------------------");
+        // apiClient.updateClient("5c9238330fb2d32bc288f1ea");
     }
 }
